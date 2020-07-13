742aac4ad40e46c59f775dac9b08354bddea9828
Anh <cu01654155443@gmail.com>
2020-07-13 18:19:03 +0700

    sua code

--- 01. Front-end/05. Excersise/TuanAnh/js/basic.js
+++ 01. Front-end/05. Excersise/TuanAnh/js/basic.js
@@ -1,25 +1,17 @@
+/*event open & close province */
 $(document).ready(function() {
-    // cài đặt sự kiện
-    setupEvent();
+    $(".province").click(function() {
+        $("#full-modal").css("display", "block");
+    });
 });
-// cài đặt sự kiện
-function setupEvent() {
-    //show
-    $("#show").click(function() {
-        $(".image").fadeIn(2000);
-    });
-    //hide
-    $("#hide").click(function() {
-        $(".image").fadeOut(2000);
-    });
-    //toggle
-    $("#toggle").click(function() {
-        $(".image").fadeToggle(500);
-    });
-    $("#ft").change(function() {
-        var x = parseFloat($(this).val());
-        $(".image").fadeTo(500, x);
 
+$(document).ready(function() {
+    $(".close-modal").click(function() {
+        $("#full-modal").css("display", "none");
     });
-
-}
+});
+$(document).ready(function() {
+    $(".accept").click(function() {
+        $("#full-modal").css("display", "none");
+    });
+});
