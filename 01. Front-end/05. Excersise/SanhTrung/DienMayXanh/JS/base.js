989cf37859ea7186fe6f780b79061cd08325a828
khung365 <nstrung163@gmail.com>
2020-08-08 01:18:20 +0700

    Message from sanhtrung project: hot promotion in day

--- 01. Front-end/05. Excersise/SanhTrung/DienMayXanh/JS/base.js
+++ 01. Front-end/05. Excersise/SanhTrung/DienMayXanh/JS/base.js
@@ -3,15 +3,34 @@ $(document).ready(function () {
   $(".province__option").click(function () {
     $(".modal").css("display", "flex");
   });
-});
-/**Modal */
-$(document).ready(function () {
+  /**Modal */
   $(".btn-close").click(function () {
     $(".modal").css("display", "none");
     $(".btn-close").css("outline", "none");
     $(".btn-close").css("border", "0");
   });
+  /**Group promotion*/
+  $(".group-promo-tabs .nav-tab-item .nav-link").on("click", function (e) {
+    e.preventDefault();
+    var $paren = $(this).parent();
+    if (!$paren.hasClass("active")) {
+      $paren.siblings().removeClass("active");
+      $paren.addClass("active");
+      var tabContentId = $(this).attr("href");
+      $(tabContentId).siblings().removeClass("active");
+      $(tabContentId).addClass("active");
+    }
+  });
 });
+
+/**Modal */
+// $(document).ready(function () {
+//   $(".btn-close").click(function () {
+//     $(".modal").css("display", "none");
+//     $(".btn-close").css("outline", "none");
+//     $(".btn-close").css("border", "0");
+//   });
+// });
 /**Carousel */
 $(".carousel").carousel({
   interval: 1000 * 5,
