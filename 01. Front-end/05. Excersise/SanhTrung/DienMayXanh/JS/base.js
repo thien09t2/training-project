1685221a6f938f0ff2e887e18bc23ea5546fa8ff
khung365 <nstrung163@gmail.com>
2020-08-08 23:38:33 +0700

    Message sanhtrung project: Dien May Xanh!

--- 01. Front-end/05. Excersise/SanhTrung/DienMayXanh/JS/base.js
+++ 01. Front-end/05. Excersise/SanhTrung/DienMayXanh/JS/base.js
@@ -21,6 +21,56 @@ $(document).ready(function () {
       $(tabContentId).addClass("active");
     }
   });
+  /**Modal overlay video list cate */
+
+  var $videoPlayer = $(".video-player-modal .video-player-body .video-player");
+  $(".btn-play-video").click(function (e) {
+    e.preventDefault();
+    let youtubeUrl = $(this).data("youtube-url");
+    if (youtubeUrl) {
+      $videoPlayer.empty();
+      $videoPlayer.append(
+        "<iframe width='100%' height='100%' src='" +
+          youtubeUrl +
+          "' frameborder='0' allowfullscreen=''></iframe>"
+      );
+      $(".video-player-modal").removeClass("d-none");
+    }
+  });
+
+  $(".video-player-modal").on("click", ".close-btn", function () {
+    $(".video-player-modal").addClass("d-none");
+    $videoPlayer.empty();
+  });
+
+  $("body").on(
+    "click",
+    ".video-player-modal:not(.video-player-body), .video-player-modal:not(.video-player)",
+    function () {
+      $(".video-player-modal").addClass("d-none");
+      $videoPlayer.empty();
+    }
+  );
+  /**Show more footer */
+  $(".showMore").click(function () {
+    $(".col-hidden").css("display", "block");
+    $(".showMore").css("display", "none");
+  });
+  $(".showMore-2").click(function () {
+    $(".col-hidden-2").css("display", "block");
+    $(".showMore-2").css("display", "none");
+  });
+  /**Scroll to top */
+  $(window).scroll(function () {
+    if ($(this).scrollTop()) {
+      $("#goto-top").fadeIn();
+    } else {
+      $("#goto-top").fadeOut();
+    }
+  });
+  $("#goto-top").click(function () {
+    $("html, body").animate({ scrollTop: 0 }, 1000);
+  });
 });
 
 /**Modal */
