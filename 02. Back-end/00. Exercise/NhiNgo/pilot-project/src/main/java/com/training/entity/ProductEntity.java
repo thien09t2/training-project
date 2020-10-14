ffbbb3d2cda69119ba257ce339e701e5bd60a278
nhingo-312 <67149884+nhingo-312@users.noreply.github.com>
2020-10-14 23:03:26 +0700

    Update pilot and dienmayxanh css

---
+++ 02. Back-end/00. Exercise/NhiNgo/pilot-project/src/main/java/com/training/entity/ProductEntity.java
@@ -0,0 +1,211 @@
+package com.training.entity;
+
+import java.util.Date;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+import javax.persistence.Transient;
+
+import org.springframework.format.annotation.DateTimeFormat;
+import org.springframework.web.multipart.MultipartFile;
+
+@Entity
+@Table(name = "PRODUCT")
+public class ProductEntity {
+
+	@Id
+	@Column(name = "PRODUCT_ID", nullable = false)
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long productId;
+
+	@Column(name = "PRODUCT_NAME", length = 100, nullable = false)
+	private String productName;
+
+	@Column(name = "QUANTITY", nullable = false)
+	private Long quantity;
+
+	@Column(name = "PRICE", nullable = false)
+	private Double price;
+
+	@JoinColumn(name = "BRAND_ID", referencedColumnName = "BRAND_ID")
+	@ManyToOne(fetch = FetchType.EAGER)
+	private BrandEntity brandEntity;
+
+	@Column(name = "SALE_DATE", nullable = false)
+	@Temporal(TemporalType.DATE)
+	@DateTimeFormat(pattern = "yyyy-MM-dd")
+	private Date saleDate;
+
+	@Column(name = "IMAGE", nullable = false)
+	private String prodImg;
+
+	@Transient
+	private MultipartFile[] prodImgFiles;
+
+	@Column(name = "DESCRIPTION", nullable = true)
+	private String prodDesct;
+
+	public ProductEntity() {
+	}
+
+//	
+//	public ProductEntity(Long productId, String productName, Long quantity, Double price, BrandEntity brandEntity,
+//			Date saleDate, String prodImg, MultipartFile[] prodImgFiles, String prodDesct) {
+//		this.productId = productId;
+//		this.productName = productName;
+//		this.quantity = quantity;
+//		this.price = price;
+//		this.brandEntity = brandEntity;
+//		this.saleDate = saleDate;
+//		this.prodImg = prodImg;
+//		this.prodImgFiles = prodImgFiles;
+//		this.prodDesct = prodDesct;
+//	}
+
+
+	/**
+	 * @param productId
+	 */
+	public ProductEntity(Long productId) {
+		super();
+		this.productId = productId;
+	}
+
+	/**
+	 * @return the productId
+	 */
+	public Long getProductId() {
+		return productId;
+	}
+
+	/**
+	 * @param productId the productId to set
+	 */
+	public void setProductId(Long productId) {
+		this.productId = productId;
+	}
+
+	/**
+	 * @return the productName
+	 */
+	public String getProductName() {
+		return productName;
+	}
+
+	/**
+	 * @param productName the productName to set
+	 */
+	public void setProductName(String productName) {
+		this.productName = productName;
+	}
+
+	/**
+	 * @return the quantity
+	 */
+	public Long getQuantity() {
+		return quantity;
+	}
+
+	/**
+	 * @param quantity the quantity to set
+	 */
+	public void setQuantity(Long quantity) {
+		this.quantity = quantity;
+	}
+
+	/**
+	 * @return the price
+	 */
+	public Double getPrice() {
+		return price;
+	}
+
+	/**
+	 * @param price the price to set
+	 */
+	public void setPrice(Double price) {
+		this.price = price;
+	}
+
+	/**
+	 * @return the brandEntity
+	 */
+	public BrandEntity getBrandEntity() {
+		return brandEntity;
+	}
+
+	/**
+	 * @param brandEntity the brandEntity to set
+	 */
+	public void setBrandEntity(BrandEntity brandEntity) {
+		this.brandEntity = brandEntity;
+	}
+
+	/**
+	 * @return the saleDate
+	 */
+//	@JsonSerialize(using = JsonSerializer.class)
+	public Date getSaleDate() {
+		return saleDate;
+	}
+
+	/**
+	 * @param saleDate the saleDate to set
+	 */
+//	@JsonSerialize(using = JsonSerializer.class)
+	public void setSaleDate(Date saleDate) {
+		this.saleDate = saleDate;
+	}
+
+	/**
+	 * @return the prodImg
+	 */
+	public String getProdImg() {
+		return prodImg;
+	}
+
+	/**
+	 * @param prodImg the prodImg to set
+	 */
+	public void setProdImg(String prodImg) {
+		this.prodImg = prodImg;
+	}
+
+	/**
+	 * @return the prodImgFiles
+	 */
+	public MultipartFile[] getProdImgFiles() {
+		return prodImgFiles;
+	}
+
+	/**
+	 * @param prodImgFiles the prodImgFiles to set
+	 */
+	public void setProdImgFiles(MultipartFile[] prodImgFiles) {
+		this.prodImgFiles = prodImgFiles;
+	}
+
+	/**
+	 * @return the prodDesct
+	 */
+	public String getProdDesct() {
+		return prodDesct;
+	}
+
+	/**
+	 * @param prodDesct the prodDesct to set
+	 */
+	public void setProdDesct(String prodDesct) {
+		this.prodDesct = prodDesct;
+	}
+
+}
---
+++ 02. Back-end/00. Exercise/NhiNgo/pilot-project/src/main/java/com/training/service/impl/ProductServiceImpl.java
@@ -0,0 +1,214 @@
+package com.training.service.impl;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import javax.transaction.Transactional;
+
+import org.apache.commons.lang3.StringUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+import com.training.common.constant.Constants;
+import com.training.common.util.FileHelpper;
+import com.training.dao.IProductDAO;
+import com.training.dao.jpaspec.ProductSpecifycation;
+import com.training.entity.ProductEntity;
+import com.training.model.PagerModel;
+import com.training.model.ResponseDataModel;
+import com.training.service.IProductService;
+
+@Service
+@Transactional
+public class ProductServiceImpl implements IProductService {
+
+	private final Logger LOGGER = LoggerFactory.getLogger(getClass());
+
+	@Value("${parent.folder.images.product}")
+	private String productImgFolderPath;
+
+	@Autowired
+	IProductDAO prodDAO;
+
+	@Override
+	public List<ProductEntity> getAllProducts() {
+		return prodDAO.findAll(Sort.by(Sort.Direction.DESC, "productId"));
+	}
+
+	@Override
+	public ProductEntity findByProductName(String productName) {
+		return prodDAO.findByProductName(productName);
+	}
+
+	@Override
+	public ProductEntity findByProductId(Long productId) {
+		return prodDAO.findByProductId(productId);
+	}
+
+	@Override
+	public ResponseDataModel findByProductIdApi(Long productId) {
+
+		int responseCode = Constants.RESULT_CD_FAIL;
+		String responseMessg = StringUtils.EMPTY;
+		ProductEntity productEntity = null;
+
+		try {
+			productEntity = prodDAO.findByProductId(productId);
+			if (productEntity != null) {
+				responseCode = Constants.RESULT_CD_SUCCESS;
+			}
+		} catch (Exception e) {
+			responseMessg = "Error occurs when finding product #" + productEntity.getProductId();
+			LOGGER.error("Error occurs when finding product " + productEntity.getProductId() + ": ", e);
+		}
+
+		return new ResponseDataModel(responseCode, responseMessg, productEntity);
+	}
+
+	@Override
+	public ResponseDataModel findAllProductsWithPagerApi(int pgNum) {
+		int responseCode = Constants.RESULT_CD_FAIL;
+		String responseMessg = StringUtils.EMPTY;
+		Map<String, Object> responseMap = new HashMap<String, Object>();
+
+		try {
+			Sort sortInfo = Sort.by(Sort.Direction.DESC, "productId");
+			Pageable pageable = PageRequest.of(pgNum - 1, Constants.PAGE_SIZE, sortInfo);
+			Page<ProductEntity> prodEntityPage = prodDAO.findAll(pageable);
+			responseMap.put("prodList", prodEntityPage.getContent());
+			responseMap.put("paginationInfo", new PagerModel(pgNum, prodEntityPage.getTotalPages()));
+			responseCode = Constants.RESULT_CD_SUCCESS;
+		} catch (Exception e) {
+			responseMessg = e.getMessage();
+			LOGGER.error("Error occurs when loading all products: ", e);
+		}
+
+		return new ResponseDataModel(responseCode, responseMessg, responseMap);
+	}
+
+	@Override
+	public ResponseDataModel addProductApi(ProductEntity productEntity) {
+
+		int responseCode = Constants.RESULT_CD_FAIL;
+		String responseMessg = StringUtils.EMPTY;
+
+		try {
+			if (findByProductName(productEntity.getProductName()) != null) {
+				responseMessg = "Product Name existed!";
+				responseCode = Constants.RESULT_CD_DUPL;
+			} else {
+				MultipartFile[] prodImgFiles = productEntity.getProdImgFiles();
+				if (prodImgFiles != null && prodImgFiles[0].getSize() > 0) {
+					String imgPath = FileHelpper.addNewFile(productImgFolderPath, prodImgFiles);
+					productEntity.setProdImg(imgPath);
+				}
+				prodDAO.saveAndFlush(productEntity);
+				responseMessg = "Product #" + productEntity.getProductId() + " has been added successfully!";
+				responseCode = Constants.RESULT_CD_SUCCESS;
+			}
+		} catch (Exception e) {
+			responseMessg = "Error occurs when adding new product!";
+			LOGGER.error("Error occurs when adding new product: ", e);
+		}
+		return new ResponseDataModel(responseCode, responseMessg);
+	}
+
+	@Override
+	public ResponseDataModel updateProductApi(ProductEntity productEntity) {
+
+		int responseCode = Constants.RESULT_CD_FAIL;
+		String responseMessg = StringUtils.EMPTY;
+
+		try {
+			ProductEntity existedProd = prodDAO.findByProductNameAndProductIdNot(productEntity.getProductName(),
+					productEntity.getProductId());
+
+			// Check if product name existed
+
+			if (StringUtils.isEmpty(productEntity.getProductName())) {
+				responseMessg = "Product Name is required!";
+			} else if (existedProd != null) {
+				responseMessg = "Product Name existed";
+				responseCode = Constants.RESULT_CD_DUPL;
+			} else {
+				MultipartFile[] prodImgFiles = productEntity.getProdImgFiles();
+				if (prodImgFiles != null && prodImgFiles[0].getSize() > 0) {
+					String imgPath = FileHelpper.editFile(productImgFolderPath, prodImgFiles,
+							productEntity.getProdImg());
+					productEntity.setProdImg(imgPath);
+				}
+				prodDAO.saveAndFlush(productEntity);
+				responseMessg = "Product #" + productEntity.getProductId() + " was modifyied successfully!";
+				responseCode = Constants.RESULT_CD_SUCCESS;
+			}
+		} catch (Exception e) {
+			responseMessg = "Error occurs when updating product #" + productEntity.getProductId() + ".";
+			LOGGER.error("Error occurs when updating product #" + productEntity.getProductId() + ": ", e);
+		}
+
+		return new ResponseDataModel(responseCode, responseMessg);
+	}
+
+	@Override
+	public ResponseDataModel deleteProductApi(Long productId) {
+
+		int responseCode = Constants.RESULT_CD_FAIL;
+		String responseMessg = StringUtils.EMPTY;
+		ProductEntity productEntity = prodDAO.findByProductId(productId);
+
+		try {
+			if (productEntity != null) {
+				prodDAO.deleteById(productId);
+				prodDAO.flush();
+
+				// Deleting Image of product after that
+				FileHelpper.deleteFile(productEntity.getProdImg());
+				responseMessg = "Product " + productEntity.getProductName() + " was deleted successfully!";
+				responseCode = Constants.RESULT_CD_SUCCESS;
+			}
+
+		} catch (Exception e) {
+			responseMessg = "Error occurs when deleting product #" + productEntity.getProductId() + ".";
+			LOGGER.error("Error occurs when deleting product #" + productEntity.getProductId() + ": ", e);
+		}
+
+		return new ResponseDataModel(responseCode, responseMessg);
+	}
+
+	@Override
+	public ResponseDataModel searchProductWithNameAndPricePager(Map<String, Object> searchConditionMap, int pgNum) {
+		int responseCode = Constants.RESULT_CD_FAIL;
+		String responseMessg = StringUtils.EMPTY;
+		Map<String, Object> responseMap = new HashMap<>();
+		
+		try {
+			Sort sortList = Sort.by(Sort.Direction.ASC,"price");
+			Pageable pageable = PageRequest.of(pgNum - 1, Constants.PAGE_SIZE, sortList);
+			Page<ProductEntity> prodEntityResultPage = prodDAO.findAll(ProductSpecifycation.getSearchCriteria(searchConditionMap), pageable);
+			responseMap.put("prodResultList", prodEntityResultPage.getContent());
+			responseMap.put("resultPaginationInfo", new PagerModel(pgNum, prodEntityResultPage.getTotalPages()));
+			responseCode = Constants.RESULT_CD_SUCCESS;
+			if (prodEntityResultPage.getTotalElements() > 0) {
+				responseMessg = "Found " + prodEntityResultPage.getTotalElements() + " matched results.";
+			} else {
+				responseMessg = "No matches were found.";
+			}
+			
+		} catch (Exception e) {
+			responseMessg = "Error occurs when loading results: " + e.getMessage();
+			LOGGER.error("Error occurs when loading results", e);
+		}
+		
+		return new ResponseDataModel(responseCode, responseMessg, responseMap);
+	}
+
+}
