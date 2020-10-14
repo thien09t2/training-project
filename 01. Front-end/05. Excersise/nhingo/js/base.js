b6572b4e6e33fd52e1c7d10a7427f48c36c58769
nhingo-312 <67149884+nhingo-312@users.noreply.github.com>
2020-10-14 18:42:26 +0700

    Add project Dienmayxanh

--- 01. Front-end/05. Excersise/nhingo/js/base.js
+++ 01. Front-end/05. Excersise/nhingo/js/base.js
@@ -1,19 +1,56 @@
-// FOr dmx-----------------
+// For dmx-----------------
 $(document).ready(function () {
-    $("#show-hide").hover(function(){
-        $(this).hide();
-        $(".toShow").show();
-    }, function(){
-        $(this).show();
-        $(".toShow").hide();
+    $("#info").on( "mouseenter", function(e) {
+        e.preventDefault();
+        $("#show-hide").removeClass("fa-caret-down");
+        $("#show-hide").addClass("fa-caret-up");
+
+    });
+    $("#info").on( "mouseleave", function(e) {
+        e.preventDefault();
+        $("#show-hide").removeClass("fa-caret-up");
+        $("#show-hide").addClass("fa-caret-down");
     });
 
-    $(".side-navbar .sup-menu").mouseover(function () {
-        $(this).parent().find(".sub-menu").css("display", "flex"
-        );
+    $(".side-navbar .sup-menu").hover(function () {
+        $(this).parent().find(".sub-menu").css("display", "flex");
+        $(this).parent().find(".sub-menu:before").css("display", "flex");
     }, function () {
-        $(this).parent().find(".sub-menu").css("display", "none"
-        );
+        $(this).parent().find(".sub-menu").css("display", "none");
+    });
+
+    $(".group-tabs .nav-tab-item .nav-link").on("click", function (e) {
+        e.preventDefault();
+        var $parent = $(this).parent();
+        if (!$parent.hasClass("active")) {
+            $parent.siblings().removeClass("active");
+            $parent.addClass("active");
+            var tabContentId = $(this).attr("href");
+            $(tabContentId).siblings().removeClass("active");
+            $(tabContentId).addClass("active");
+        }
+    });
+
+    var $videoPlayer = $(".vid-player-modal .vid-player-body .vid-player");
+    $('.btn-play').click(function (e) {
+        e.preventDefault();
+        let youtubeUrl = $(this).data("youtube-url");
+        if (youtubeUrl) {
+            $videoPlayer.empty();
+            $videoPlayer.append("<iframe width='100%' height='100%' src='" + youtubeUrl + " '" +
+                "frameborder='0' allowfullscreen=''></iframe>");
+            $(".vid-player-modal").removeClass("d-none");
+        }
+    });
+
+    $(".vid-player-modal").on("click", ".close-btn", function () {
+        $(".vid-player-modal").addCLass("d-none");
+        $videoPlayer.empty();
+    });
+    $("body").on("click", ".vid-player-modal:not(.vid-player-body)," +
+        ".vid-player-modal:not(.vid-player)", function () {
+        $(".vid-player-modal").addClass("d-none");
+        $videoPlayer.empty();
     });
 
 
@@ -39,8 +76,4 @@ function topFunction() {
     document.documentElement.scrollTop = 0; //Chrome, IE, ...
 }
 
-/* w3School Top button */
 
-function knhFunction() {
-    x.classList.toggle("fa-caret-up")
-}
